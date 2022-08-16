ab1df8ea695292842017a4dda85f9f7ff7371e66
ernest-phillips <ernest-phillips@github.com>
2022-08-16 11:37:28 -0700

    generate models

---
+++ app/controllers/loans_controller.rb
@@ -0,0 +1,4 @@
+class LoansController < ApplicationController
+  def new
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,3 @@
-class User < ActiveRecord::Base
-  attr_accessor :email, :password, :first_name, :last_name, :password_confirmation
-
-  def initialize(email, password, first_name, last_name, password_confirmation)
-    @email = email
-    @password = password
-    @first_name = first_name
-    @last_name = last_name
-    # @password_confirmation = password_confirmation
-  end
+class User < ApplicationRecord
+  
 end
