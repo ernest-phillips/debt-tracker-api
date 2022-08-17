18a1cd2d832824ecc0ba3cab5a2487a4d2f08faa
ernest-phillips <ernest-phillips@github.com>
2022-08-17 13:14:36 -0700

    add email validation to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  REGEX_PATTERN = /^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$/
+  def is_email_valid? email
+      email =~REGEX_PATTERN
+  end
 end
