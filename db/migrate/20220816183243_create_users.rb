c2ddeb9a69d5dddcef8874ac1c4af6c08a5c379d
ernest-phillips <ernest-phillips@github.com>
2022-08-17 13:06:58 -0700

    remove non-resourceful routes

--- db/migrate/20220816183243_create_users.rb
+++ db/migrate/20220816183243_create_users.rb
@@ -1,11 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
+      t.string :email , null: false, unique: true
       t.string :password
-      t.string :first_name
-      t.string :last_name
+      t.string :first_name, null: false
+      t.string :last_name, null: false
 
       t.timestamps
     end
