f5f91e04911c70f1f352779f6b271ff7e0d84e88
ernest-phillips <ernest-phillips@github.com>
2022-08-19 18:48:19 -0700

    Creditor migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_16_183243) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_20_014418) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "creditors", force: :cascade do |t|
+    t.string "creditor_name"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
