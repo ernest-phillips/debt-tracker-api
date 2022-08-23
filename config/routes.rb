deb29175da93eb6da7c294346fbfcab5d3d1acef
ernest-phillips <ernest-phillips@github.com>
2022-08-23 14:28:16 -0700

    Update route with resourceful creditors

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'creditors/index'
-  get 'creditors/show'
-  get 'creditors/create'
-  get 'creditors/update'
-  get 'creditors/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :users, only: %i[create]
+  resources :creditors, only: %i[index show create update destroy]
 end
