3a75e37375f5d1cad1bd26a865c94bb311bcde76
ernest-phillips <ernest-phillips@github.com>
2022-08-23 12:59:12 -0700

    update user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,4 @@
 
 class User < ApplicationRecord
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validate :email_validation
-
-  def email_validation
-    if email.blank?
-      errors.add(:email, "can't be blank")
-    end
-  end
 end
