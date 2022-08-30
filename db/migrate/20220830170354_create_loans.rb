b71a96d05432488828f661072d0f02e544e86c97
ernest-phillips <ernest-phillips@github.com>
2022-08-30 10:09:34 -0700

    create loan model

---
+++ db/migrate/20220830170354_create_loans.rb
@@ -0,0 +1,15 @@
+class CreateLoans < ActiveRecord::Migration[7.0]
+  def change
+    create_table :loans do |t|
+      t.string :name
+      t.bigint :balance
+      t.bigint :min_payment
+      t.bigint :interest
+      t.description :text
+      t.references :user, null: false, foreign_key: true
+      t.references :creditor, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
