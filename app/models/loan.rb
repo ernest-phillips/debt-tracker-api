fff2d72b26019a18490a89e46791cca53cc9928e
ernest-phillips <ernest-phillips@github.com>
2022-09-15 10:03:59 -0700

    update model associations

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,2 +1,6 @@
 class Loan < ApplicationRecord
+  belongs_to :user
+  belongs_to :creditor
+
+  has_many :loans 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@
 class User < ApplicationRecord
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
 
+  has_many :loans
+  # has_many :creditors, through: :loans
 end
