5358c1b97b7144bc2da74dd08d382d7b04ff0645
ernest-phillips <ernest-phillips@github.com>
2022-09-26 11:03:32 -0700

    Create new endpoints for loans

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,5 +1,22 @@
 class LoansController < ApplicationController
   def create
+    Loans.create!(loan_params)
+    render json: { message: 'Loan created' }, status: :created
+  end
+
+  def index
+    render json: Loan.all
+  end
+
+  def update
+    loan = Loan.find(params[:id])
+    loan.update!(loan_params)
+    render json: { message: 'Loan updated' }, status: :ok
+  end
+
+  def destroy
+    Loan.find(params[:id]).destroy
+    render json: { message: 'Loan deleted' }, status: :ok
   end
 
   private
