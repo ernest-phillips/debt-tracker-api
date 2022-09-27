52964dde5bdb493504b8d8fb46cf71b7012185b5
ernest-phillips <ernest-phillips@github.com>
2022-09-27 12:38:31 -0700

    setup loans test variables

--- spec/requests/loans_spec.rb
+++ spec/requests/loans_spec.rb
@@ -2,12 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'Loans', type: :request do
   describe 'POST /loans' do
-   before do
-     @user = create(:user)
-     @creditor = create(:creditor)
-     @loan = create(:loan)
-   end
-   
+
+    let(:loan) { create(:loan) }
+    let(:creditor) { create(:creditor) }
+    let(:user) { create(:user) }
+    let(:name) { 'New Loan Name' }
     let(:loan_params) do
       {
         loan: {
@@ -16,8 +15,8 @@ RSpec.describe 'Loans', type: :request do
           min_payment: 50,
           interest: 0.05,
           description: 'My student loan',
-          creditor_id: @creditor.id,
-          user_id: @user.id
+          creditor_id: creditor.id,
+          user_id: user.id
         }
       }
     end
@@ -28,18 +27,20 @@ RSpec.describe 'Loans', type: :request do
 
     it 'deletes a loan' do
       loan = create(:loan)
-      expect{ delete("/loans/#{@loan.id}") }.to change { Loan.count }.by(-1)
+      expect{ delete("/loans/#{loan.id}") }.to change { Loan.count }.by(-1)
     end
 
     it 'updates a loan' do
-      loan_params[:loan][:name] = 'New Loan Name'
-      put("/loans/#{@loan.id}", params: loan_params)
-      expect(@loan.reload.name).to eq('New Loan Name')
+      loan_params[:loan][:name] = name
+      put("/loans/#{loan.id}", params: loan_params)
+      expect(loan.reload.name).to eq(name)
     end
 
-    it 'returns a list of loans' do
+    it 'returns a list of loans' do      
+      loans = create_list(:loan, 3)
       get('/loans')
       expect(response).to have_http_status(:ok)
+      expect(response.body).to eq(loans.to_json)
     end
   end
 end
