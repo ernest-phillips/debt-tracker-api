1d602f237b52d3a513ac36e1cb7c6bde292b6d60
ernest-phillips <ernest-phillips@github.com>
2022-09-27 10:22:50 -0700

    use instance variables

--- spec/factories/loans.rb
+++ spec/factories/loans.rb
@@ -2,7 +2,7 @@ require 'faker'
 
 FactoryBot.define do
   factory :loan do
-    name { Faker::Company.name }
+    name { Faker::Company.buzzword }
     balance { Faker::Number.decimal(l_digits: 2) }
     min_payment { Faker::Number.decimal(l_digits: 2) }
     interest { Faker::Number.decimal(l_digits: 2) }
--- spec/requests/loans_spec.rb
+++ spec/requests/loans_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'Loans', type: :request do
    before do
      @user = create(:user)
      @creditor = create(:creditor)
+     @loan = create(:loan)
    end
    
     let(:loan_params) do
@@ -27,18 +28,16 @@ RSpec.describe 'Loans', type: :request do
 
     it 'deletes a loan' do
       loan = create(:loan)
-      expect{ delete("/loans/#{loan.id}") }.to change { Loan.count }.by(-1)
+      expect{ delete("/loans/#{@loan.id}") }.to change { Loan.count }.by(-1)
     end
 
     it 'updates a loan' do
-      loan = create(:loan)
       loan_params[:loan][:name] = 'New Loan Name'
-      put("/loans/#{loan.id}", params: loan_params)
-      expect(loan.reload.name).to eq('New Loan Name')
+      put("/loans/#{@loan.id}", params: loan_params)
+      expect(@loan.reload.name).to eq('New Loan Name')
     end
 
     it 'returns a list of loans' do
-      create(:loan)
       get('/loans')
       expect(response).to have_http_status(:ok)
     end
