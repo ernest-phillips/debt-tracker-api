80bf383a18bbe50ead42c6b4e2059668733fc129
ernest-phillips <ernest-phillips@github.com>
2022-09-27 10:10:29 -0700

    add more CRUD tests to loans

--- spec/requests/loans_spec.rb
+++ spec/requests/loans_spec.rb
@@ -24,5 +24,23 @@ RSpec.describe 'Loans', type: :request do
     it 'creates a new loan' do
       expect{ post('/loans', params: loan_params) }.to change { Loan.count }.by(1)
     end
+
+    it 'deletes a loan' do
+      loan = create(:loan)
+      expect{ delete("/loans/#{loan.id}") }.to change { Loan.count }.by(-1)
+    end
+
+    it 'updates a loan' do
+      loan = create(:loan)
+      loan_params[:loan][:name] = 'New Loan Name'
+      put("/loans/#{loan.id}", params: loan_params)
+      expect(loan.reload.name).to eq('New Loan Name')
+    end
+
+    it 'returns a list of loans' do
+      create(:loan)
+      get('/loans')
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
