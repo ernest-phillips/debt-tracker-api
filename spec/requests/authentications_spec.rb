e1421215689cb34c7e10ca350f43812ad2add73c
ernest-phillips <ernest-phillips@github.com>
2022-10-11 12:26:34 -0700

    fix forbidden access test

--- spec/requests/authentications_spec.rb
+++ spec/requests/authentications_spec.rb
@@ -5,6 +5,7 @@ require 'rails_helper'
 RSpec.describe 'Authentications', type: :request do
   describe 'POST /authentications' do
     let(:user) { create(:user) }
+    let(:other_user) { create(:user) }
     let(:authentication_params) do
       {
         authentication: {
@@ -27,16 +28,15 @@ RSpec.describe 'Authentications', type: :request do
     end
 
     it 'responds with 401 status code if user is not authenticated' do
-      post('/login', params: authentication_params)
-
-      get('/users/:id', headers: { 'Authorization' => 'invalid token' })
+      get('/users/3', headers: { 'Authorization' => 'invalid token' })
 
       expect(response).to have_http_status(401)
     end
 
     it 'responds with 403 status if user is forbidden' do
       post('/login', params: authentication_params)
-      get '/users/:id'
+      token = JSON.parse(response.body)['token']
+      get "/users/#{other_user.id}", headers: { 'Authorization' => token }
 
       expect(response).to have_http_status(403)
     end
